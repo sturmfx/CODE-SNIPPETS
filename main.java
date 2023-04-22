da96f11f18a7792881cd7ad70036a2a7613cf8d3
sturmfx <ttunikitashtobert@gmail.com>
2023-04-22 15:13:35 +0300

    Update main.java

--- main.java
+++ main.java
@@ -20,3 +20,30 @@ public void tick()
             player.setX(player.getX() - MOVE_PER_TICK);
         }
     }
+
+
+public void spawnBot()
+    {
+        if(bots.size() < 3)
+        {
+            boolean controller = true;
+            int x = 0;
+            int y = 0;
+            Bot b1 = null;
+            while(controller)
+            {
+                x = rand.nextInt(WIDTH);
+                y = rand.nextInt(HEIGHT);
+                for(Rectangle1 r1: rec1)
+                {
+                    if(!(r1.p2.getX() > x && r1.p1.getX() < x && r1.p3.getY() < y && r1.p2.getY() > y))
+                    {
+                        controller = false;
+                        break;
+                    }
+                }
+            }
+            b1 = new Bot(x, y, 10);
+            bots.add(b1);
+        }
+    }
